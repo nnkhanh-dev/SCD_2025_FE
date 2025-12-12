f51dbad0fb57fa4ce796e7b5a64b38a94284a044
NguyenNhatKhanh <nguyennhatkhanh151203@gmail.com>
2025-12-13 02:08:52 +0700

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { toast } from "vue3-toastify";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
@@ -150,7 +150,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
